7fa89da34f6e96c28d6b13d64af1f1a89b336c54
unknown <ddoucett@ucsd.edu>
2021-03-25 18:10:07 -0700

    new commit

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,7 +3,6 @@ var tableData = data;
 
 var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
-
 var city = d3.select("#city");
 var columns = ["date/time", "city", "state", "country", "shape","comment"]
 
@@ -24,5 +23,18 @@ button.on("click", function() {
     tbody.html("");
     var date = d3.select("#datetime");
     var input = date.property("value");
-    console.log(inputValue)
+    console.log(input)
+    var filteredData = tableData.filter(sighting => sighting.datetime === input);
+    console.log(filteredData);
+
+    filteredData.forEach(function(selections) {
+      console.log(selections);
+
+      var row = tbody.append("tr");
+      Object.entries(selections).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
 });
