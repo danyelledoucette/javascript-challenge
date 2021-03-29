c392678212f6a0ae57966fcb469b88fc701b2681
unknown <ddoucett@ucsd.edu>
2021-03-28 19:33:08 -0700

    ufo_2 edits

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -4,7 +4,9 @@ var tableData = data;
 var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var city = d3.select("#city");
-var columns = ["date/time", "city", "state", "country", "shape","comment"]
+var state = d3.select("#state");
+var country = d3.select("#country");
+var shape = d3.select("#shape");
 
 tableData.forEach(function(ufoSighting) {
     console.log(ufoSighting);
@@ -23,9 +25,20 @@ button.on("click", function() {
     tbody.html("");
     var date = d3.select("#datetime");
     var input = date.property("value");
+
     console.log(input)
     var filteredData = tableData.filter(sighting => sighting.datetime === input);
+    var filteredData = tableData.filter(sighting => sighting.city === input);
+    var filteredData = tableData.filter(sighting => sighting.state === input);
+    var filteredData = tableData.filter(sighting => sighting.country === input);
+    var filteredData = tableData.filter(sighting => sighting.shape === input);
+
+                                                    // sighting.city === input ||
+                                                    // sighting.state === input ||
+                                                    // sighting.country === input ||
+                                                    // sighting.shape === input);
     console.log(filteredData);
+
 
     filteredData.forEach(function(selections) {
       console.log(selections);
